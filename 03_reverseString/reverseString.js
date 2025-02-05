96389c582d6cc473ac0f72522fee1c11144f42d8
root <vitorsvierszc@gmail.com>
2025-02-05 12:07:45 -0300

    Solved third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let wordArray = string.split("")
+    let wordArrayCopy = string.split("")
+    let arrayLength = wordArray.length;
+    let revArray = ("")
+    while(arrayLength > 0) {
+       revArray = revArray + wordArray.pop(wordArrayCopy.length -1);
+        arrayLength = arrayLength - 1
+    }
+    return revArray
 };
 
 // Do not edit below this line
