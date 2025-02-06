b8778ce4a19a31c6a7e27bbb057d0e61b8b95983
root <vitorsvierszc@gmail.com>
2025-02-06 17:01:20 -0300

    Solved exercise 05, way easier than the last one (although im done who made it difficult) stackexchange helped a lot

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+const sumAll = function(firstInt, lastInt) {
+    if(parseFloat(firstInt) > 0 && parseFloat(lastInt) > 0 && Number.isInteger(firstInt)==true && Number.isInteger(lastInt)== true){
+    const numArray = []
+    const secondArray = [firstInt, lastInt]
+    secondArray.sort()
+    let minItem = Math.min(secondArray[0])
+    let maxItem = Math.max(secondArray[1])
+    
+    for(i = minItem; i <= maxItem; i++){
+        numArray.push(i);
+    }
+    
+    
+    return numArray.reduce((a, b) => a + b, 0)}
+    else{return("ERROR")}
+} 
+    
+    // Do not edit below this line
+    module.exports = sumAll;
