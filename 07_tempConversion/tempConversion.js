361ae2c4d69d9c3494e3c3cd1915490b656e1fa1
root <vitorsvierszc@gmail.com>
2025-02-07 12:35:16 -0300

    solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+degreeF = (degreeF - 32)*5/9;
+degreeF = (degreeF * 10 / 10).toFixed(1);
+degreeF = parseFloat(degreeF);
+return degreeF;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  degreeC = (9/5 * degreeC) + 32;
+  degreeC = (degreeC * 10 / 10).toFixed(1);
+  degreeC = parseFloat(degreeC)
+  return degreeC;
 };
 
 // Do not edit below this line
